20b316b3963a719d8c71fbae43621ed7becb6c45
K e n V <kenneth_valdivia@yahoo.com>
2019-12-28 23:22:47 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,3 +43,30 @@ def naming_system
   # HoH all the way to Species!
 
 end
+
+  family = {
+   :label => "Family",
+   :sub_category => {
+     :label => "Genus",
+     :sub_category => {
+         :label => "Species",
+   :sub_category => nil
+     }
+   }
+ }
+    order = {
+    :label => "Order", 
+    :sub_category => family
+  }
+     domain = {
+   :label => "Kingdom",
+    :sub_category => {
+      :label => "Phylum",
+      :sub_category => {
+        :label => "Class",
+         :sub_category => order 
+     }
+   }
+ }
+   domain
+ end
